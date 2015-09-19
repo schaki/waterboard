d37525af2814d0d5a54dc812e91315119b187e01
Sayan Chaki <sayan.chaki@gmail.com>
2015-09-19 10:31:20 -0400

    Working depth sensor reading

--- waterboard_dev.ino
+++ waterboard_dev.ino
@@ -90,6 +90,33 @@ long microsecondsToInches(long microseconds) {
   return microseconds / 74 / 2;
 }
 
+unsigned long pulseIn(uint16_t pin, uint8_t state) {
+
+    GPIO_TypeDef* portMask = (PIN_MAP[pin].gpio_peripheral); // Cache the target's peripheral mask to speed up the loops.
+    uint16_t pinMask = (PIN_MAP[pin].gpio_pin); // Cache the target's GPIO pin mask to speed up the loops.
+    unsigned long pulseCount = 0; // Initialize the pulseCount variable now to save time.
+    unsigned long loopCount = 0; // Initialize the loopCount variable now to save time.
+    unsigned long loopMax = 20000000; // Roughly just under 10 seconds timeout to maintain the Spark Cloud connection.
+
+    // Wait for the pin to enter target state while keeping track of the timeout.
+    while (GPIO_ReadInputDataBit(portMask, pinMask) != state) {
+        if (loopCount++ == loopMax) {
+            return 0;
+        }
+    }
+
+    // Iterate the pulseCount variable each time through the loop to measure the pulse length; we also still keep track of the timeout.
+    while (GPIO_ReadInputDataBit(portMask, pinMask) == state) {
+        if (loopCount++ == loopMax) {
+            return 0;
+        }
+        pulseCount++;
+    }
+
+    // Return the pulse time in microseconds by multiplying the pulseCount variable with the time it takes to run once through the loop.
+    return pulseCount * 0.405; // Calculated the pulseCount++ loop to be about 0.405uS in length.
+}
+
 void loop() {
 
   /*if(digitalRead(pirPin) == HIGH){
